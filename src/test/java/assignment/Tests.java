8aaa6441290925fbf5c29f5aef1ca3de2de79d6f
Zack Osowski <62389933+zackosowski@users.noreply.github.com>
2020-11-25 10:21:55 -0600

    Initial commit

---
+++ src/test/java/assignment/Tests.java
@@ -0,0 +1,29 @@
+package assignment;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+//import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.params.ParameterizedTest;
+//import org.junit.jupiter.params.provider.CsvSource;
+
+//import java.io.*;
+
+
+public class Tests {
+
+
+   @Test
+   public void code(){
+       App.main(null);
+       //assertEquals("EXPECTED", App.testMethod(), "Code Failed");
+   }
+
+   @Test
+    public void late() {
+        App.main(null);
+        //assertTrue(DueDate.onTime(2020, 11, 24), "Submitted Late");
+    }
+
+}
